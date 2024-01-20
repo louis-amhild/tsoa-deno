61480803ca0dbf1de0966902dec97dc3dff5d119
Louis Amhild <la@storyhunt.dk>
2024-01-20 11:53:26 +0100

    feat(deno): Add deno:build command and ignore deno_dist folder in build process

---
+++ packages/runtime/denoify.post-build.js
@@ -0,0 +1,35 @@
+/**
+ * Copy template services from tsoa/cli to Deno dist tsoa/runtime package
+ */
+const path = require('path');
+const fs = require('fs');
+const templatesDir = './../cli/src/routeGeneration/templates';
+const targetDir = './deno_dist/routeGeneration';
+const indexTsPath = './deno_dist/index.ts';
+
+const DenoTemplateServices = [
+    'templateService.ts',
+    'hono/honoTemplateService.ts',
+]
+
+// Copy service files and add export statements to package index
+function copyAndAddExports() {
+    let exportStatements = '// Dynamically added template services from tsoa/cli to Deno tsoa/runtime\n';
+    
+    DenoTemplateServices.forEach((file) => {
+        const sourcePath = path.join(templatesDir, file);
+        const targetPath = path.join(targetDir, file);
+        // Ensure target directory exists
+        fs.mkdirSync(path.dirname(targetPath), { recursive: true });
+        fs.copyFileSync(sourcePath, targetPath);
+        exportStatements += `export * from './routeGeneration/${file.replace('.ts', '')}';\n`;
+    });
+
+    // Append export statements to deno_dist/index.ts
+    fs.appendFileSync(indexTsPath, exportStatements);
+}
+
+// Execute the function to perform the tasks
+copyAndAddExports();
+
+console.log('Denoify post build completed.');

09f2cf9a232dd474db8dd86e7f2f558431427720
Louis Amhild <la@storyhunt.dk>
2024-01-25 20:51:23 +0100

    feat(deno): Implement Hono hbs template

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./packages/**/tsconfig.json', './tests/tsconfig.json', './tests/esm/tsconfig.json'],
+    project: ['./packages/**/tsconfig.json', './packages/**/deno.json', './tests/tsconfig.json', './tests/esm/tsconfig.json'],
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],

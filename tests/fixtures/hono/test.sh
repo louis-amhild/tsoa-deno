f401073065e1ba61694065cbba0ff1c32a49ca29
Louis Amhild <la@storyhunt.dk>
2024-02-21 14:10:39 +0100

    chore(tsoa-deno): Add standalone prepare hono script

---
+++ tests/fixtures/hono/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# Shortcut helper script for fast test and debugging of hono template changes
+
+# Link honoTemplateService source to deno_dist to avoid rebuilding deno_dist on every change
+# 0. ln packages/cli/src/routeGeneration/templates/hono/honoTemplateService.ts packages/runtime/deno_dist/routeGeneration/hono/honoTemplateService.ts
+
+# 1. Copy Hono template definition to runtime cli
+cp ../../../packages/cli/src/routeGeneration/templates/hono.hbs ../../../packages/cli/dist/routeGeneration/templates
+
+# 2. Generate routes.ts
+yarn prepare-test:hono
+
+# 3. Run tests
+yarn test:deno
---
+++ tests/prepare-hono.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+import { dim, green } from 'chalk';
+import { generateSpecAndRoutes, generateRoutes } from '../packages/cli';
+import { Timer } from './utils/timer';
+
+const spec = async () => {
+  const result = await generateSpecAndRoutes({
+    configuration: 'tsoa.json',
+  });
+  return result;
+};
+
+const log = async <T>(label: string, fn: () => Promise<T>) => {
+  console.log(dim(green(`↻ Starting ${label}...`)));
+  const timer = new Timer();
+
+  const result = await fn();
+  console.log(green(`✓ Finished ${label} in ${timer.elapsed()}ms`));
+
+  return result;
+};
+
+(async () => {
+  const metadata = await log('Swagger Spec Generation', spec);
+
+  await Promise.all([
+    log('Hono Route Generation', () =>
+      generateRoutes({
+        noImplicitAdditionalProperties: 'silently-remove-extras',
+        bodyCoercion: true,
+        authenticationModule: './fixtures/hono/authentication.ts',
+        basePath: '/v1',
+        entryFile: './fixtures/hono/server.ts',
+        middleware: 'hono',
+        routesDir: './fixtures/hono',
+      }),
+    ),
+    log('Hono Route Generation (but noImplicitAdditionalProperties is set to "throw-on-extras")', () =>
+      generateRoutes({
+        noImplicitAdditionalProperties: 'throw-on-extras',
+        bodyCoercion: true,
+        authenticationModule: './fixtures/hono/authentication.ts',
+        basePath: '/v1',
+        entryFile: './fixtures/hono/serverNoAdditional.ts',
+        middleware: 'hono',
+        routesDir: './fixtures/hono',
+        routesFileName: 'routesNoAdditional.ts',
+      }),
+    ),
+  ]);
+})();

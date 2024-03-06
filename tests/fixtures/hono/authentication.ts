68cf12eaab1a71a506afe05f440a4ee9ec6f3f14
Louis Amhild <la@storyhunt.dk>
2024-03-06 22:39:23 +0100

    feat(deno): Implement Hono Deno tests

---
+++ tests/fixtures/hono/authentication.ts
@@ -0,0 +1,36 @@
+import { HonoRequest } from '@hono';
+
+interface Request extends Omit<HonoRequest, 'query'> {
+  query: Record<string, string | string[]>;
+}
+
+export function honoAuthentication(request: Request, name: string, scopes?: string[]): Promise<any> {
+  if (name === 'api_key') {
+    let token;
+    if (request.query && request.query.access_token) {
+      token = request.query.access_token;
+    }
+
+    if (token === 'abc123456') {
+      return Promise.resolve({
+        id: 1,
+        name: 'Ironman',
+      });
+    } else if (token === 'xyz123456') {
+      return Promise.resolve({
+        id: 2,
+        name: 'Thor',
+      });
+    } else {
+      return Promise.reject({ message: 'api_key' });
+    }
+  } else if (name === 'slow_auth') {
+    return new Promise((_, reject) => setTimeout(() => reject({ message: 'slow_auth' }), 10000));
+  } else {
+    if (request.query && request.query.tsoa && request.query.tsoa === 'abc123456') {
+      return Promise.resolve({});
+    } else {
+      return Promise.reject({ message: 'other' });
+    }
+  }
+}

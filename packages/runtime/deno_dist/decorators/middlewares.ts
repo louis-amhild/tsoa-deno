8f075620a8abb0fefad8154335712a4f4b6d85a0
actions <actions@github.com>
2024-03-09 18:11:44 +0000

    Add TSOA runtime Deno distribution files

---
+++ packages/runtime/deno_dist/decorators/middlewares.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type Middleware<T extends Function | Object> = T;
+
+const TSOA_MIDDLEWARES = Symbol('@tsoa:middlewares');
+
+/**
+ * Helper function to create a decorator
+ * that can act as a class and method decorator.
+ * @param fn a callback function that accepts
+ *           the subject of the decorator
+ *           either the constructor or the
+ *           method
+ * @returns
+ */
+function decorator(fn: (value: any) => void) {
+  return (...args: any[]) => {
+    // class decorator
+    if (args.length === 1) {
+      fn(args[0]);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    } else if (args.length === 3 && args[2].value) {
+      // method decorator
+      const descriptor = args[2] as PropertyDescriptor;
+      if (descriptor.value) {
+        fn(descriptor.value);
+      }
+    }
+  };
+}
+
+/**
+ * Install middlewares to the Controller or a specific method.
+ * @param middlewares
+ * @returns
+ */
+export function Middlewares<T extends Function | Object>(...mws: Array<Middleware<T>>): ClassDecorator & MethodDecorator {
+  return decorator(target => {
+    if (mws) {
+      const current = fetchMiddlewares<T>(target);
+      Reflect.defineMetadata(TSOA_MIDDLEWARES, [...current, ...mws], target);
+    }
+  });
+}
+
+/**
+ * Internal function used to retrieve installed middlewares
+ * in controller and methods (used during routes generation)
+ * @param target
+ * @returns list of middlewares
+ */
+export function fetchMiddlewares<T extends Function | Object>(target: any): Array<Middleware<T>> {
+  return Reflect.getMetadata(TSOA_MIDDLEWARES, target) || [];
+}
---
+++ packages/runtime/deno_dist/routeGeneration/templateService.ts
@@ -0,0 +1,27 @@
+import { Controller, TsoaRoute, ValidationService } from "@tsoa/runtime";
+
+export abstract class TemplateService<ApiHandlerParameters, ValidationArgsParameters, ReturnHandlerParameters> {
+  protected validationService: ValidationService;
+
+  constructor(
+    protected readonly models: TsoaRoute.Models,
+  ) {
+    this.validationService = new ValidationService(models);
+  }
+
+  abstract apiHandler(params: ApiHandlerParameters): Promise<any>;
+
+  abstract getValidatedArgs(params: ValidationArgsParameters): any[];
+
+  protected abstract returnHandler(params: ReturnHandlerParameters): any;
+
+  protected isController(object: Controller | Object): object is Controller {
+    return 'getHeaders' in object && 'getStatus' in object && 'setStatus' in object;
+  }
+
+  protected buildPromise(methodName: string, controller: Controller | Object, validatedArgs: any) {
+    const prototype = Object.getPrototypeOf(controller);
+    const descriptor = Object.getOwnPropertyDescriptor(prototype, methodName);
+    return descriptor!.value.apply(controller, validatedArgs);
+  }
+}

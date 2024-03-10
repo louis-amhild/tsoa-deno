4e6119830e6dde1ed59c6400dd564b89be346095
Louis Amhild <la@storyhunt.dk>
2024-03-11 00:16:38 +0100

    feat(tsoa-deno): Implement deno example

--- deno-example/example/index.ts
+++ deno-example/example/index.ts
@@ -1,17 +1,9 @@
 // Get TSOA controller routes for this endpoint
-import * as routes from "../_shared/routes.ts";
+import * as routes from '../_shared/routes.ts';
 
-import { Hono } from 'https://deno.land/x/hono/mod.ts';
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-
-
-import { assertEquals } from "https://deno.land/std/assert/assert_equals.ts";
-import { delay } from "https://deno.land/std/async/delay.ts";
-
-const x = 1 + 2;
-assertEquals(x, 3);
+import { Hono } from '@hono';
 
 const app = new Hono();
 // Register routes
 routes.RegisterRoutes(app);
-serve(app.fetch);
+Deno.serve(app.fetch);

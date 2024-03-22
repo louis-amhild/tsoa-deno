a23fe8571c79242737d634cd82cce0f92a3ad5aa
actions <actions@github.com>
2024-03-22 22:27:45 +0000

    Add TSOA runtime Deno distribution files

---
+++ packages/runtime/deno_dist/routeGeneration/templates/templateService.ts
@@ -0,0 +1,29 @@
+import { Controller } from '../../interfaces/controller.ts';
+import { TsoaRoute } from '../tsoa-route.ts';
+import { ValidationService } from '../templateHelpers.ts';
+
+export abstract class TemplateService<ApiHandlerParameters, ValidationArgsParameters, ReturnHandlerParameters> {
+  protected validationService: ValidationService;
+
+  constructor(
+    protected readonly models: TsoaRoute.Models,
+  ) {
+    this.validationService = new ValidationService(models);
+  }
+
+  abstract apiHandler(params: ApiHandlerParameters): Promise<any>;
+
+  abstract getValidatedArgs(params: ValidationArgsParameters): any[];
+
+  protected abstract returnHandler(params: ReturnHandlerParameters): any;
+
+  protected isController(object: Controller | Object): object is Controller {
+    return 'getHeaders' in object && 'getStatus' in object && 'setStatus' in object;
+  }
+
+  protected buildPromise(methodName: string, controller: Controller | Object, validatedArgs: any) {
+    const prototype = Object.getPrototypeOf(controller);
+    const descriptor = Object.getOwnPropertyDescriptor(prototype, methodName);
+    return descriptor!.value.apply(controller, validatedArgs);
+  }
+}
